2a17126eb808dda2861cc88aa3db8a4550146c65
Joaquin58 <ramirezmendozajoaquinrodrigo@gmail.com>
2022-03-31 10:31:32 -0600

    feat(api/routes index): add endpoints to create users POST /user

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,76 @@
 const { Router } = require('express');
+const axios = require('axios');
+const { Op } = require('Sequelize')
+const { Roles, Users, BanckCards } = require('../db')
 
 
 const router = Router();
 
+async function insert(name, email, password, roleid) {
+    const [newUser, create] = await Users.create(
+        {
+            name,
+            email,
+            password,
+         
+        },
+    )
+    
+    //     console.log(roleid)
+    //     console.log(newUser)
+    //     await newUser.addRoles(roleid)
+    //     return newUser
+    
+}
 
+
+async function getbyEmail(pEmail) {
+    const matched = await Users.findAll({
+        where: {
+            email: pEmail
+        }
+    })
+    return matched
+}
+
+
+router.post('/user', async (req, res) => {
+    const { name, email, password, roleid } = req.body
+    try {
+        if (await getbyEmail(email).length > 0) {
+            return res.json(await getbyEmail(email))
+        } else {
+            return res.json(await insert(name, email, password, roleid))
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/users', async (req, res) => {
+    const users = await Users.findAll({
+        include: [{
+            model: Roles,
+            attributes: ['rolName'],
+            through: {
+                attributes: []
+            }
+        }]
+    });
+    res.json(users)
+})
+
+router.post('/roles', async (req, res) => {
+    const { id, name } = req.body
+    const newUser = await Roles.create(
+        {   id,
+            rolName: name
+        }
+    )
+    return res.json(newUser)
+})
+
+router.get('/roles', async (req, res) => {
+    res.json(await Roles.findAll())
+})
 module.exports = router;
