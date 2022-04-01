4f0c94af48577292c3d48e293396b633549e4f17
sergio2448 <84557725+sergio2448@users.noreply.github.com>
2022-04-01 11:33:06 -0500

    updates

--- api/src/controller/PropertiesController.js
+++ api/src/controller/PropertiesController.js
@@ -1,26 +1,48 @@
-const { Properties,Features,Photos } = require('../db')
-const { Op } = require("sequelize") 
+const { Properties, Features, Photos } = require("../db");
+const { Op } = require("sequelize");
 
 const getProperties = async (name) => {
-    try {
-        let respProperties;
-        if(name){
-            respProperties= await Properties.findAll({
-                include:[{model:Features},{model:Photos}],
-                where:{
-                    name:name.toLowerCase(),
-                }
-            });
-        }else{
-            respProperties=await Properties.findAll({  include:[{model:Features},{model:Photos}]})
-        }
-        return respProperties;
-
-    } catch (error) {
-        console.log("Ocurrio un error en PropertiesController/ getProperties:"+error);
+  try {
+    let respProperties;
+    if (name) {
+      respProperties = await Properties.findAll({
+        include: [{ model: Features }, { model: Photos }],
+        where: {
+          name: name.toLowerCase(),
+        },
+      });
+    } else {
+      respProperties = await Properties.findAll({
+        include: [{ model: Features }, { model: Photos }],
+      });
     }
-}
+    return respProperties;
+  } catch (error) {
+    console.log(
+      "Ocurrio un error en PropertiesController/ getProperties:" + error
+    );
+  }
+};
 
-module.exports ={
-    getProperties
-}
+const fillProperties = async (req, res) => {
+  try {
+    let { description, m2, address, city, country, cost, cp, lease } = req.body;
+    let newProperty = await Properties.create({
+      description,
+      m2,
+      address,
+      city,
+      country,
+      cost,
+      cp,
+      lease,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = {
+  getProperties,
+  fillProperties
+};
