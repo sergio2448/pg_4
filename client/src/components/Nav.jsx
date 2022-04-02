4a3ef03b9fa4ade020115bf8abdcb721ee717659
cluz123 <pmlizarraga@gmail.com>
2022-04-02 13:55:33 -0300

    card y update de home

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Nav(){
     return (
@@ -11,8 +11,8 @@ function Nav(){
             <Link className='text-center text-lg transition ease-in-out duration-200 hover:bg-sky-500 px-3 py-2 rounded' to='/'>Contact</Link>
           </div>
           <div className='text-right'>
-            <Link className='mr-5 transition ease-in-out duration-300 hover:border-b-sky-500 border-b-transparent hover:border-b-2 py-1 ' to='/'>Register</Link>
-            <Link className='mr-10 transition ease-in-out duration-300 hover:border-b-sky-500 border-b-transparent hover:border-b-2 py-1 ' to='/'>Log in</Link>
+            <a onClick={()=> {alert('Coming Soon')}} className='mr-5 transition ease-in-out duration-300 hover:border-b-sky-500 border-b-transparent hover:border-b-2 py-1 ' to='/'>Register</a>
+            <a onClick={()=> {alert('Coming Soon')}} className='mr-10 transition ease-in-out duration-300 hover:border-b-sky-500 border-b-transparent hover:border-b-2 py-1 ' to='/'>Log in</a>
           </div>
         </nav>
     )
