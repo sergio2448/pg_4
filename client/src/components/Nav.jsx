bfa091531f8aaad9e4b43ad6309b29dd5351730b
AgusVelazquez <mateokcyo@gmail.com>
2022-04-03 04:57:24 -0300

    Auth0React

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { LoginButton } from './Auth0/Login'
+import { LogoutButton } from './Auth0/Logout'
+import { Profile } from './Auth0/Profile'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Nav(){
+  const { isAuthenticated } = useAuth0();
+
+
     return (
         <nav className='pt-5 font-Monserrat text-white relative grid grid-cols-3 gap-4 z-100 w-full'>
           <span className='ml-10 text-xl font-semibold'>INMOBILIARIA</span>
@@ -11,8 +18,12 @@ function Nav(){
             <Link className='text-center text-lg transition ease-in-out duration-200 hover:bg-sky-500 px-3 py-2 rounded' to='/'>Contact</Link>
           </div>
           <div className='text-right'>
-            <Link className='mr-5 transition ease-in-out duration-300 hover:border-b-sky-500 border-b-transparent hover:border-b-2 py-1 ' to='/'>Register</Link>
-            <Link className='mr-10 transition ease-in-out duration-300 hover:border-b-sky-500 border-b-transparent hover:border-b-2 py-1 ' to='/'>Log in</Link>
+            {isAuthenticated? <>
+            <Profile/>
+            <LogoutButton/>
+            </>
+            :<LoginButton/>
+}
           </div>
         </nav>
     )
