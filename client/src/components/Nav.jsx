345479388a06844fd7e85f93df855c2f427a92c1
AgusVelazquez <mateokcyo@gmail.com>
2022-04-03 18:01:51 -0300

    Update NavLink

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { LoginButton } from './Auth0/Login'
 import { LogoutButton } from './Auth0/Logout'
 import { Profile } from './Auth0/Profile'
