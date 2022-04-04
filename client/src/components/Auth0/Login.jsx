3fe5bf960fbc23ed40b9d1fc455d2018cc31d776
cluz123 <pmlizarraga@gmail.com>
2022-04-04 15:24:23 -0300

    asdas

--- client/src/components/Auth0/Login.jsx
+++ client/src/components/Auth0/Login.jsx
@@ -4,5 +4,5 @@ import  { useAuth0 } from '@auth0/auth0-react';
 export const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
-    return <button onClick={()=> loginWithRedirect()}>Log in or Sign up</button>
+    return <button className="text-base mr-5 transition ease-in-out duration-300 hover:border-b-sky-500 border-b-transparent hover:border-b-2 pt-1" onClick={()=> loginWithRedirect()}>Log in or Sign up</button>
 };
