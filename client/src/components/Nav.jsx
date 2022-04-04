5c2f328822f08e7d8c95a68b860474a99b437902
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2022-04-04 04:16:19 -0300

    refactor nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -6,17 +6,12 @@ import { Profile } from './Auth0/Profile'
 import { useAuth0 } from '@auth0/auth0-react'
 
 function Nav() {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
-
+  const { isAuthenticated } = useAuth0();
 
   return (
     <nav className='pt-5 font-Monserrat text-white relative grid grid-cols-3 gap-4 z-100 w-full'>
       <span className='ml-10 text-xl font-semibold'>INMOBILIARIA</span>
       <div className='m-auto space-x-6'>
-
-
-
-        
         <NavLink className={({ isActive }) =>
           (isActive ? "bg-sky-500 px-3 py-2 rounded" : "text-center text-lg transition ease-in-out duration-200 hover:bg-sky-500 px-3 py-2 rounded")
         } to='/state/create'>About us</NavLink>
