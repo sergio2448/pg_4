2408e12d305393e5443db6c97e17d89405d64f2a
Erwin C <erwin.sistemas@gmail.com>
2022-04-05 22:43:21 -0500

    ruta getFeature

---
+++ api/src/controller/getFeatures.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const { Users } = require('../db')
+const router = Router();
+
+
+router.get('/', async (req, res) => {
+   
+})
+
+module.exports = router;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,6 +5,7 @@ const postroles = require('../controller/postroles.js')
 const postcards = require('../controller/postcard.js')
 const getroles = require('../controller/getroles.js')
 const PropertiesRoute = require('../controller/PropertiesRoute');
+const getFeatures = require('../controller/getFeatures')
 
 const router = Router();
 router.use("/Properties", PropertiesRoute);
@@ -23,5 +24,8 @@ router.use('/roles', getroles)
 
 //todo: crea una tarjeta y asocia segun el nombre
 router.use('/cards', postcards)
+
+//todo: crea una tarjeta y asocia segun el nombre
+router.use('/feature', getFeatures)
 
 module.exports = router;
