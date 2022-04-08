6a1421ecd8bd5a7ca67b995ae611fe78c686b4b6
sergio2448 <84557725+sergio2448@users.noreply.github.com>
2022-04-08 18:33:43 -0500

    update

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -2,63 +2,90 @@ import React from "react";
 import hardcodeHouse from "../../styles/images/hardcode-house.jpg";
 import seller from "../../styles/images/seller.png";
 import Nav from "../Nav";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import {getHomeDetail} from '../../redux/actions/index'
+import { getHomeDetail } from "../../redux/actions/index";
+import Modal1 from "./Modal1";
 
-const Detail = ({ name, city, country, cost, measure, rooms }) => {
+const Detail = ({ name, city, country, cost, measure, rooms, description }) => {
   name = "Hardcode Street";
   city = "Hardcode City";
   country = "Hardcode Country";
   cost = "$4000 usd";
   measure = "300 sq m";
   rooms = 5;
+  description =
+    "A perfect place to rest, in a very quiet neighborhood, 5 minutes walk from some places of interest (Parque Principal, Cancha, Malecón, Terminal de buses), located in the urban area, with the possibility of parking in front of the accommodation and system security 24/7.";
 
-  let {id}       = useParams();
-  const dispatch  = useDispatch();
-  const detail   = useSelector(state => state.homeDetail);
-  
+  let { id } = useParams();
+  const dispatch = useDispatch();
+  const detail = useSelector((state) => state.homeDetail);
 
   useEffect(() => {
-     dispatch(getHomeDetail(id));
-    return () => {dispatch(getHomeDetail([]))}
-    },[id])
+    dispatch(getHomeDetail(id));
+    return () => {
+      dispatch(getHomeDetail([]));
+    };
+  }, [id]);
 
-    console.log(detail);
+  console.log(detail);
   return (
     <div className=" text-center ">
       <div className="bg-[#075985]">
-      <div className='bg-black shadow-nav h-20 relative z-20'>
-                <Nav />
-            </div>
+        <div className="bg-black shadow-nav h-20 relative z-20">
+          <Nav />
+        </div>
       </div>
       <h2 className="mt-6 text-stone-600 text-5xl font-base font-Poppins">
-        <strong>{name}</strong>
+        <strong>
+          {detail[0]?.country},{detail[0]?.city}
+        </strong>
       </h2>
       <div className="mx-32 px-5 mt-12 grid grid-cols-4 gap-6">
+        <Modal1 />
         <div>
           <img
             className="border-2 h-full object-cover transition ease-in-out duration-200 hover:opacity-60"
-            src={detail[0]?.photos?.length>0?'http://localhost:3001/Properties/images/'+detail[0].photos[0].photos:hardcodeHouse}
+            src={
+              detail[0]?.photos?.length > 0
+                ? "http://localhost:3001/Properties/images/" +
+                  detail[0].photos[0].photos
+                : hardcodeHouse
+            }
           />
         </div>
         <div>
           <img
             className="w-60 border-2 object-cover transition ease-in-out duration-200 hover:opacity-60"
-            src={detail[0]?.photos?.length>1?'http://localhost:3001/Properties/images/'+detail[0].photos[1].photos:hardcodeHouse}
+            src={
+              detail[0]?.photos?.length > 1
+                ? "http://localhost:3001/Properties/images/" +
+                  detail[0].photos[1].photos
+                : hardcodeHouse
+            }
           />
           <img
             className="w-60 border-2 object-cover transition ease-in-out duration-200 hover:opacity-60"
-            src={detail[0]?.photos?.length>2?'http://localhost:3001/Properties/images/'+detail[0].photos[2].photos:hardcodeHouse}
+            src={
+              detail[0]?.photos?.length > 2
+                ? "http://localhost:3001/Properties/images/" +
+                  detail[0].photos[2].photos
+                : hardcodeHouse
+            }
           />
         </div>
         <div>
           <img
             className="w-60 border-2 object-cover transition ease-in-out duration-200 hover:opacity-60"
-            src={detail[0]?.photos?.length>3?'http://localhost:3001/Properties/images/'+detail[0].photos[3].photos:hardcodeHouse}
+            src={
+              detail[0]?.photos?.length > 3
+                ? "http://localhost:3001/Properties/images/" +
+                  detail[0].photos[3].photos
+                : hardcodeHouse
+            }
           />
-          
+
           <img
             className="w-60 border-2 object-cover transition ease-in-out duration-200 hover:opacity-60"
             src={hardcodeHouse}
@@ -75,22 +102,81 @@ const Detail = ({ name, city, country, cost, measure, rooms }) => {
           />
         </div>
       </div>
-      <div className="mx-32 px-6 mt-12 grid grid-cols-3 gap-6 mb-6">
+      <div className="mx-32 px-6 mt-12 grid grid-cols-4 gap-6 mb-6">
         <div>
           <h5 className="text-center">
-            <strong>City:</strong> {city}
-          </h5>
-          <h5 className="text-center">
-            <strong>Country:</strong> {country}
+            Measure: <strong> {detail[0]?.m2 ? detail[0].m2 : 45} m²</strong>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              enable-background="new 0 0 24 24"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+            >
+              <g>
+                <rect fill="none" height="24" width="24" />
+              </g>
+              <g>
+                <g>
+                  <path d="M17.66,17.66l-1.06,1.06l-0.71-0.71l1.06-1.06l-1.94-1.94l-1.06,1.06l-0.71-0.71l1.06-1.06l-1.94-1.94l-1.06,1.06 l-0.71-0.71l1.06-1.06L9.7,9.7l-1.06,1.06l-0.71-0.71l1.06-1.06L7.05,7.05L5.99,8.11L5.28,7.4l1.06-1.06L4,4v14c0,1.1,0.9,2,2,2 h14L17.66,17.66z M7,17v-5.76L12.76,17H7z" />
+                </g>
+              </g>
+            </svg>
           </h5>
         </div>
         <div>
           <h5 className="text-center">
-            <strong>Cost:</strong> {cost}{" "}
+            Rooms:
+            <strong>
+              {detail[0]?.features[0]?.produc_features.value
+                ? detail[0]?.features[0]?.produc_features.value
+                : 2}
+            </strong>
           </h5>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            enable-background="new 0 0 24 24"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <g>
+              <rect fill="none" height="24" width="24" />
+            </g>
+            <g>
+              <path d="M22,12c0-1.1-0.9-2-2-2V7c0-1.1-0.9-2-2-2H6C4.9,5,4,5.9,4,7v3c-1.1,0-2,0.9-2,2v5h1.33L4,19h1l0.67-2h12.67L19,19h1 l0.67-2H22V12z M18,10h-5V7h5V10z M6,7h5v3H6V7z M4,12h16v3H4V12z" />
+            </g>
+          </svg>
+        </div>
+        <div>
           <h5 className="text-center">
-            <strong>Measure:</strong> {measure}{" "}
+            Bathrooms:
+            <strong>
+              {detail[0]?.features[1]?.produc_features.value
+                ? detail[0]?.features[1]?.produc_features.value
+                : 1}
+            </strong>
           </h5>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            enable-background="new 0 0 24 24"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <g>
+              <rect fill="none" height="24" width="24" />
+            </g>
+            <g>
+              <g>
+                <circle cx="7" cy="7" r="2" />
+                <path d="M20,13V4.83C20,3.27,18.73,2,17.17,2c-0.75,0-1.47,0.3-2,0.83l-1.25,1.25C13.76,4.03,13.59,4,13.41,4 c-0.4,0-0.77,0.12-1.08,0.32l2.76,2.76c0.2-0.31,0.32-0.68,0.32-1.08c0-0.18-0.03-0.34-0.07-0.51l1.25-1.25 C16.74,4.09,16.95,4,17.17,4C17.63,4,18,4.37,18,4.83V13h-6.85c-0.3-0.21-0.57-0.45-0.82-0.72l-1.4-1.55 c-0.19-0.21-0.43-0.38-0.69-0.5C7.93,10.08,7.59,10,7.24,10C6,10.01,5,11.01,5,12.25V13H2v6c0,1.1,0.9,2,2,2c0,0.55,0.45,1,1,1h14 c0.55,0,1-0.45,1-1c1.1,0,2-0.9,2-2v-6H20z M20,19H4v-4h16V19z" />
+              </g>
+            </g>
+          </svg>
         </div>
         <div>
           <div className="grid grid-cols-2 gap-3">
@@ -104,14 +190,34 @@ const Detail = ({ name, city, country, cost, measure, rooms }) => {
           </button>
         </div>
       </div>
+
       <hr />
       <div className="py-8 px-6 mb-6 text-xl font-bold font-Monserrat">
         <p className="mb-6">
-          A perfect place to rest, in a very quiet neighborhood, 5 minutes walk
-          from some places of interest (Parque Principal, Cancha, Malecón,
-          Terminal de buses), located in the urban area, with the possibility of
-          parking in front of the accommodation and system security 24/7.
+          {detail[0]?.description ? detail[0]?.description : description}
         </p>
+        <hr />
+        <div>
+          <h3>Main data of the property</h3>
+        </div>
+        <div className="mx-32 px-6 mt-12 grid grid-cols-3 gap-6 mb-6">
+          <div>
+            <h5 className="text-center">
+              <strong>City:</strong> {city}
+            </h5>
+            <h5 className="text-center">
+              <strong>Country:</strong> {country}
+            </h5>
+          </div>
+          <div>
+            <h5 className="text-center">
+              <strong>Cost:</strong> {cost}{" "}
+            </h5>
+            <h5 className="text-center">
+              <strong>Measure:</strong> {measure}{" "}
+            </h5>
+          </div>
+        </div>
         <hr />
         <div>
           <h3 className="px-6 mt-6 mb-6 text-xl font-bold font-Monserrat">
