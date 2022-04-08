c4f5974dee84bda63675cd04b6bf3a74d3ee9a49
Joaquin58 <ramirezmendozajoaquinrodrigo@gmail.com>
2022-04-08 14:47:51 -0500

    fix(controllers/sendemail): controllers y transporter arreglado para pruebas

--- api/src/controller/sendemails.js
+++ api/src/controller/sendemails.js
@@ -14,46 +14,51 @@ module.exports = router
 
 router.post("/", async (req, res) => {
     const { userid, postStatus } = req.body
-    const user = await getUserById(userid)
-    const emailUser = await getEmailUser(user)
-    console.log(emailUser)
-    const userName = getUserName(user)
-    console.log(userName)
+    try {
+        const user = await getUserById(userid)
+        const emailUser = await getEmailUser(user)
+        console.log(emailUser)
+        const userName = getUserName(user)
+        console.log(userName)
+        
+        const handlebarsOptions = {
+            viewEngine: {
+                extName: '.handlebars',
+                partialsDir: './views',
+                defaultLayout: false,
+            },
+            viewPath: './views',
+            extName: ".handlebars",
+        }
     
-    const handlebarsOptions = {
-        viewEngine: {
-            extName: '.handlebars',
-            partialsDir: './views',
-            defaultLayout: false,
-        },
-        viewPath: './views',
-        extName: ".handlebars",
-    }
-
-    transporter.use('compile', hbs(handlebarsOptions));
-
-    await transporter.verify()
-
-    let mailOptions = {
-        from: '"Publicación completa 👻" <dominicode.xyz@gmail.com>',
-        to: emailUser,
-        subject: 'Testing and Testing',
-        template: postStatus,
-        context: {
-            Document:`Publicacion completa`,
-            title: `Hola ${userName},`,
+        transporter.use('compile', hbs(handlebarsOptions));
+    
+        await transporter.verify()
+    
+        let mailOptions = {
+            from: '"Publicación completa 👻" <dominicode.xyz@gmail.com>',
+            to: emailUser,
+            subject: 'Testing and Testing',
+            template: postStatus,
+            context: {
+                Document:`Publicacion completa`,
+                title: `Hola ${userName},`,
+            }
+    
         }
-
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.log(err)
+                return res.status(500).send(err.message)
+            } else {
+                console.log("email enviado")
+                return res.status(200).json(info.response)
+            }
+        })
+    } catch (error) {
+        res.status(500).json(error)
     }
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            console.log(err)
-            return res.status(500).send(err.message)
-        } else {
-            console.log("email enviado")
-            return res.status(200).json(info.response)
-        }
-    })
+   
 
 
 })
--- api/src/middlewares/transporterconfig.js
+++ api/src/middlewares/transporterconfig.js
@@ -2,18 +2,19 @@ const nodemailer = require('nodemailer')
 const { MY_EMAIL, PASSWORD } = process.env
 
 let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    service: 'gmail',
-    port: 465,
-    secure: true,
+    host: "smtp.mailtrap.io",
+    // service: 'gmail',
+    port: 2525,
+    // secure: true,
     auth: {
         user: MY_EMAIL,
         pass: PASSWORD
-    },
-    tls: {
-        rejectUnauthorized: true,
-        minVersion: "TLSv1.2"
     }
+    // ,
+    // tls: {
+    //     rejectUnauthorized: true,
+    //     minVersion: "TLSv1.2"
+    // }
 })
 
 module.exports = transporter
