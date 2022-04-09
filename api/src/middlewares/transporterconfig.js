182b55387cf598cd163ad8f6a440b7beb8f16d91
Joaquin58 <ramirezmendozajoaquinrodrigo@gmail.com>
2022-04-09 12:59:35 -0500

    fix(app/sendmail): arreglo de la ruta de emails

--- api/src/middlewares/transporterconfig.js
+++ api/src/middlewares/transporterconfig.js
@@ -1,21 +1,26 @@
 const nodemailer = require('nodemailer')
-const { MY_EMAIL, PASSWORD } = process.env
+const nodemailerSendgrid = require('nodemailer-sendgrid')
+const { MY_EMAIL, PASSWORD, API_KEY } = process.env
 
-let transporter = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    // service: 'gmail',
-    port: 2525,
-    // secure: true,
-    auth: {
-        user: MY_EMAIL,
-        pass: PASSWORD
-    }
-    // ,
-    // tls: {
-    //     rejectUnauthorized: true,
-    //     minVersion: "TLSv1.2"
-    // }
-})
-
+// let transporter = nodemailer.createTransport({
+//     host: "smtp.mailtrap.io",
+//     // service: 'gmail',
+//     port: 2525,
+//     // secure: true,
+//     auth: {
+//         user: MY_EMAIL,
+//         pass: PASSWORD
+//     }
+//     // ,
+//     // tls: {
+//     //     rejectUnauthorized: true,
+//     //     minVersion: "TLSv1.2"
+//     // }
+// })
+const transporter = nodemailer.createTransport(
+    nodemailerSendgrid({
+        apiKey: API_KEY
+    })
+)
 module.exports = transporter
-    
+
