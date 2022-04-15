1198c91d11a50c39b36a4e35b58037d159b7dea4
sergio2448 <84557725+sergio2448@users.noreply.github.com>
2022-04-15 18:12:53 -0500

    deploy

--- api/src/middlewares/configS3.js
+++ api/src/middlewares/configS3.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const fs = require('fs')
 const S3 = require('aws-sdk/clients/s3')
 
-const bucketName = process.env.AWS_BUCKET_NAME
-const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_KEY
+const { AWS_BUCKET_NAME, AWS_BUCKET_REGION, AWS_ACCESS_KEY, AWS_SECRET_KEY } =
+  process.env;
+
+const bucketName = AWS_BUCKET_NAME;
+const region = AWS_BUCKET_REGION;
+const accessKeyId = AWS_ACCESS_KEY;
+const secretAccessKey = AWS_SECRET_KEY;
 
 const s3 = new S3({
   region,
