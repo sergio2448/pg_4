30df47af095fd0bdbd428e3bd88b6deb09a514ef
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2022-04-19 18:08:03 -0300

    fix sesion init, schedule, dropdown admin

--- api/src/controller/getAgenda.js
+++ api/src/controller/getAgenda.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const { getAgenda, postAgenda } = require('../middlewares/CalendarMidd')
+const { Agenda } = require("../db")
 const router = Router();
 
 
-router.post('/', async (req, res) => {
+router.put('/', async (req, res) => {
     try {
         const { idSeller, idBuyer } = req.body;
         console.log(idSeller, idBuyer);
@@ -26,6 +27,25 @@ router.post('/', async (req, res) => {
         createCita?.dataValues ?
         res.status(200).json(createCita?.dataValues)
         :res.status(404).json({message:"No se pudo crear la cita."});
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message:error.message});
+    }
+})
+
+router.put('/status', async (req, res) => {
+    try {
+        const { id, status } = req.body;
+        const changeStatus = await Agenda.findByPk(id);
+        console.log(changeStatus)
+        if(changeStatus) {
+            changeStatus.status = status
+            await changeStatus.save()
+        }
+        res.status(200).send({
+            status: "ok",
+            changeStatus
+        })
     } catch (error) {
         console.log(error.message);
         res.status(500).send({message:error.message});
