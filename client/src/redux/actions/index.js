c8092c54044d7d55d40f53ffd29aa6519895a622
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2023-01-15 20:02:59 -0600

    fix: fixed api conection

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "${API_URL}"
+const API_URL = "pg4-production.up.railway.app"
 export function getSearchbar(input, body) {
     console.log(body)
     return async (dispatch) => {
